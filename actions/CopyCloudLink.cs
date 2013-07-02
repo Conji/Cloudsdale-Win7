f2bbe5e8bd9cfb9794f03dc693f1550a067f7842
Jake <colorswirl@outlook.com>
2013-07-02 17:00:03 -0400

    Fixed.

---
+++ actions/CopyCloudLink.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using Cloudsdale;
+using Cloudsdale.connection;
+using Cloudsdale.lib;
+using Newtonsoft.Json.Linq;
+
+namespace Cloudsdale.actions
+{
+    class CopyCloudLink
+    {
+        public static void FetchCloudLink(ICollection<JToken> clouds)
+        {
+            Main instance = new Main();
+            var Index = instance.CloudList.SelectedIndices.ToString();
+            foreach (var cloud in clouds)
+                if (instance.CloudList.FocusedItem.Selected.ToString() != null)
+                {
+                    if (cloud["name"].ToString() == instance.CloudList.FocusedItem.ToString())
+                    {
+                        Clipboard.SetText(Endpoints.Cloud.Replace("[:id]", cloud["id"].ToString()));
+                        MessageBox.Show("Link copied to clipboard!");
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Could not fetch link!");
+                }
+        }
+    }
+}
