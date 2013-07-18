93447fcd99a533d4b9fcbe6f153d95c3ae52f4b7
Conji <epicneasy@gmail.com>
2013-07-18 00:02:21 -0400

    Updating da sheet

---
+++ lib/connection/CouldNotLoginException.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json.Linq;
+
+namespace Cloudsdale.connection
+{
+    public class CouldNotLoginException : Exception
+    {
+        private JToken data;
+        public CouldNotLoginException(string responseData)
+        {
+            data = JObject.Parse(responseData);
+        }
+
+        public override string Message
+        {
+            get
+            {
+                return "An error occured while attempting to log in! Check to make sure your username and password are correct!";
+            }
+        }
+    }
+}
