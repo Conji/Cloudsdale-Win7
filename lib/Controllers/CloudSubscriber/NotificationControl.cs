039a87515f327f117f4238a4a8a6e497da23bcd0
Conji <epicneasy@gmail.com>
2013-07-19 01:34:43 -0400

    meh

---
+++ lib/Controllers/CloudSubscriber/NotificationControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Cloudsdale.lib.Controllers.CloudSubscriber
+{
+    class NotificationControl
+    {
+        private static NotifyIcon _subscriber = new NotifyIcon();
+        public static void 
+    }
+}
