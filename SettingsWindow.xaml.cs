12ba4a9bb73f8e6b033be79e7ca06c9f2e2cb2f4
Conji <dubstepandponies95@gmail.com>
2013-08-02 17:43:38 -0400

    Polished the settings and added a username check.

--- SettingsWindow.xaml.cs
+++ SettingsWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -11,6 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Shapes;
+using Cloudsdale_Win7.Assets;
 using Cloudsdale_Win7.Models;
 using Cloudsdale_Win7.Cloudsdale;
 
@@ -22,6 +24,8 @@ namespace Cloudsdale_Win7
     public partial class SettingsWindow : Window
     {
         public static SettingsWindow Instance;
+        private static Regex NameRegex = new Regex(@"([A-Za-z0-9_]+)", RegexOptions.IgnoreCase);
+
         public SettingsWindow()
         {
             InitializeComponent();
@@ -39,15 +43,53 @@ namespace Cloudsdale_Win7
             }
             aka.Text += MainWindow.User["user"]["also_known_as"].ToString().MultiReplace("[", "]", "\"", "");
         }
-
-        public void CloseSettings(object sender, RoutedEventArgs e)
-        {
-            Instance.Visibility = Visibility.Collapsed;
-        }
-
         private void name_TextChanged(object sender, TextChangedEventArgs e)
         {
-            
+            if (name.IsFocused)
+            {
+                var input = name.Text;
+                var hasSymbol = !String.IsNullOrEmpty(input) && !Char.IsLetterOrDigit(input[input.Length - 1]);
+
+                if (hasSymbol)
+                {
+                    var b = new SolidColorBrush();
+                    b.Color = Cloudsdale_Source.Error_Bright;
+                    name.Background = b;
+                }
+                else
+                {
+                    var b = new SolidColorBrush();
+                    b.Color = Cloudsdale_Source.Success_Bright;
+                    name.Background = b;
+                }
+            }
+        }
+        private void Logout(object sender, RoutedEventArgs e)
+        {
+            MainWindow.Instance.Frame.Navigate(new Login());
+            Login.Instance.EmailBox.Text = UserSettings.Default.PreviousEmail;
+            Login.Instance.PasswordBox.Password = UserSettings.Default.PreviousPassword;
+            Login.Instance.autoSession.IsChecked = false;
+            MainWindow.Instance.CloudList.ItemsSource = null;
+            MainWindow.User = null;
+            MainWindow.Instance.CloudList.Width = 5;
+        }
+
+        private void CheckLength(object sender, RoutedEventArgs e)
+        {
+            if (newPassword.Password.Length >= 6)
+            {
+                var b = new SolidColorBrush();
+                b.Color = Cloudsdale_Source.Success_Bright;
+                newPassword.Background = b;
+            }
+            else
+            {
+                var b = new SolidColorBrush();
+                b.Color = Cloudsdale_Source.Error_Bright;
+                newPassword.Background = b;
+            }
         }
     }
 }
+ 
